b1a079e8feeac4a19d720fc71746cb42a4b2f143
Rahul <97725003+Rahul9649@users.noreply.github.com>
2022-01-26 13:00:43 +0530

    Update concate.java

--- concate.java
+++ concate.java
@@ -3,12 +3,12 @@ class concate
   public static void main(String args[])
    {
    concate Obj=new concate();
-   Obj.consum("5"+"6");
+   Obj.consum(5,6);
    Obj.consum("Rahul"+" "+"V"+" "+"Pillai");
    }
-  void consum(int a)
+  void consum(int c,int b)
   {
-   System.out.println("Concated number:" +a);
+   System.out.println("Concated number:" +c+b);
   }
   void consum(String a)
    {
